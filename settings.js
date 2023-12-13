7b84a3e1fbb364a507000f654c4b79db8d1a5067
XZEROO01 <anjaigta01@gmail.com>
2023-12-13 14:57:21 +0700

    FAFA

---
+++ settings.js
@@ -0,0 +1,83 @@
+const chalk = require("chalk")
+const fs = require("fs")
+
+
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+
+//if api key expire, u can generate one from here: https://beta.openai.com/account/api-keys
+global.keyopenai = "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"
+
+//batas
+global.APIKeys = {
+    'https://api.shinoa.xyz/docs': '451C1A14'
+}
+
+//owmner v card
+global.owner = ['628'] //ur owner number
+global.ownernomer = "628" //ur owner number2
+global.ownername = "Queen Elisa MD" //ur owner name
+global.namaku = "Syarifah Firah Az Zahra A.Sy"
+global.ytname = "Queen Elisa" //ur yt chanel name
+global.socialm = "Jangan spam bot atau owner bot" //ur github or insta name
+global.location = "Asia/Jakarta" //ur location
+
+//new
+global.botname = "Queen Elisa MD"
+global.ownernumber = '628'
+global.ownername = 'Elisa Cantip'
+global.ownerNumber = ["62895320660901@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@queenelisabot"
+global.websitex = "https://youtube.com/@queenelisabot"
+global.wagc = "https://youtube.com/@queenelisabot"
+global.themeemoji = '🚩'
+global.wm = "Created By Queen Elisa MD"
+global.wmbot = "Created By Queen Elisa MD"
+global.botscript = 'Sc Ini Private🤣?' //script link
+global.packname = "©Sticker"
+global.author = "Created By Queen Elisa MD"
+global.creator = "628@s.whatsapp.net"
+global.prefa = ['/','!','.','#','&']
+global.hituet = 0
+global.running = "Panel Pterodactyl"
+//media target
+global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
+global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+
+//menu image maker
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//messages
+global.mess = {
+    success: 'Nih',
+    admin: 'This feature could be used by admins only!',
+    botAdmin: 'Bot Must Be Admin First!',
+    premime: 'Premium Special Features If You Want to Register Type Rent',
+    owner: 'This feature could be used by owner only',
+    group: 'Features Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This feature could be used by bot only',
+    wait: 'proses...',
+    linkm: 'Where is the link?',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
